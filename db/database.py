b44779bbb00c85a223212cfcdd353b1c499a110e
Behrooz Fili <behrooz.filzadeh@gmail.com>
2026-01-28 11:41:00 +0100

    Complete database setup and User model

--- db/database.py
+++ db/database.py
@@ -1,8 +1,8 @@
-from sqlachemy.ext.declarative import declarative_base
-from sqlachemy.orm import sessionmaker
-from sqlachemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "sqlite:///Instagram.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from db.database import engine, Base
+import db.models as models
+
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 def  Home():
